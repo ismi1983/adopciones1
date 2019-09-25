3153fb364c7ac602061e2c970135ba6b26f51ff7
Hector Soto <hectors.bts@gmail.com>
2019-09-25 16:08:29 -0500

    First version of routing

--- give-me-a-roof/src/app/app-routing.module.ts
+++ give-me-a-roof/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DogsComponent } from './components/dogs/dogs.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    // component: Migue's component,
+  },
+  {
+    path: 'dogs',
+    component: DogsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
