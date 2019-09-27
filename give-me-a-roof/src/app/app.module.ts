bdcfa56303ef0048087f57d4d38155a07ab49f27
Miguel <miguelv.bts@gmail.com>
2019-09-27 17:17:40 -0500

    Log in validation applied, in case a error prints a alert

--- give-me-a-roof/src/app/app.module.ts
+++ give-me-a-roof/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
  
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,6 +24,8 @@ import { LoginComponent } from './components/login/login.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- give-me-a-roof/src/app/components/login/login.component.ts
+++ give-me-a-roof/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', [Validators.required])
+  });
+
+  constructor(private routing: Router) { }
 
   ngOnInit() {
   }
 
+  login(data): void{
+    if(this.loginForm.valid){
+      this.routing.navigate(['/']);
+    }else{
+      if(!this.loginForm.get('email').valid && this.loginForm.get('password').valid){
+        alert("El correo proporcionado es incorrecto.")
+      }else if(!this.loginForm.get('password').valid && this.loginForm.get('email').valid){
+        alert("La contraseña proporcionada es incorrecta.")
+      }else{
+        alert("Los datos proporcionados son incorrectos");
+      }
+    }
+  }
 }
