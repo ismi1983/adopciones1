2052622fbf605408327486370be57079321f731a
Miguel <miguelv.bts@gmail.com>
2019-09-27 17:28:00 -0500

    Changes for more readability applied

--- give-me-a-roof/src/app/components/login/login.component.ts
+++ give-me-a-roof/src/app/components/login/login.component.ts
@@ -9,26 +9,26 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  loginForm: FormGroup = new FormGroup({
+  loginForm: FormGroup = new FormGroup ({
     email: new FormControl('', [Validators.email, Validators.required]),
     password: new FormControl('', [Validators.required])
   });
 
-  constructor(private routing: Router) { }
+  constructor (private routing: Router) { }
 
-  ngOnInit() {
+  ngOnInit () {
   }
 
-  login(data): void{
-    if(this.loginForm.valid){
+  login (): void {
+    if (this.loginForm.valid) {
       this.routing.navigate(['/']);
-    }else{
-      if(!this.loginForm.get('email').valid && this.loginForm.get('password').valid){
+    } else {
+      if (!this.loginForm.get('email').valid  &&  this.loginForm.get('password').valid) {
         alert("El correo proporcionado es incorrecto.")
-      }else if(!this.loginForm.get('password').valid && this.loginForm.get('email').valid){
+      } else if (!this.loginForm.get('password').valid  &&  this.loginForm.get('email').valid) {
         alert("La contraseña proporcionada es incorrecta.")
-      }else{
-        alert("Los datos proporcionados son incorrectos");
+      } else {
+        alert("Los datos proporcionados son incorrectos.");
       }
     }
   }
