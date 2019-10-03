bb50f2b2c3a3aa00a2645520c14a0189454c79d1
Hector Soto <hectors.bts@gmail.com>
2019-10-03 16:41:38 -0500

    Styling of Sign Up

--- give-me-a-roof/src/app/app-routing.module.ts
+++ give-me-a-roof/src/app/app-routing.module.ts
@@ -20,7 +20,7 @@ const routes: Routes = [
   {
     path: 'dogs/create',
     component: CreateDogComponent,
-    // canActivate: [AuthGuard],
+    canActivate: [AuthGuard],
   },
   {
     path: 'login',
